f3aa8e7c2dbbc3b82fd686a86e856e20a881f308
ninad99 <ninuch7@gmail.com>
2020-03-26 17:02:37 +0530

    TODO -> Add auth routes

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+// TODO
+
+// /login -> GET
+
+// /login -> POST
+
+// /signup -> GET
+
+// /signup -> POST
+
+// /logout -> POST
+
+// /password-reset -> GET
+
+// /password-reset -> POST
+
+// /password-reset/:token -> GET
+
+// /new-password -> POST
