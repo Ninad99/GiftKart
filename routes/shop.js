a5d968a0012fc80bf7af26800642f97f958e0731
ninad99 <ninuch7@gmail.com>
2020-03-26 17:01:54 +0530

    Initial shop routes added

---
+++ routes/shop.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const shopController = require('../controllers/shop');
+
+// / -> GET (Default route)
+router.get('/', shopController.getIndex);
+
+// /products -> GET (View products)
+router.get('/products', shopController.getProducts);
+
+// /products/:productId -> GET (View individual product)
+router.get('/products/:productId', shopController.getProduct);
+
+// /cart -> GET (View user cart)
+
+// /cart -> POST
+
+// /card-delete-item -> POST
+
+// /orders -> GET (View user orders)
+
+// /create-order -> POST
+
+module.exports = router;
