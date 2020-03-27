1aea696e8bb00886ca948c93fd5a91c00facd953
ninad99 <ninuch7@gmail.com>
2020-03-27 12:36:30 +0530

    login/signup/logout routes added with form validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,55 @@
-// TODO
+const express = require('express');
+const { body } = require('express-validator');
+
+const authController = require('../controllers/auth');
+const User = require('../models/user');
+
+const router = express.Router();
 
 // /login -> GET
+router.get('/login', authController.getLogin);
 
 // /login -> POST
+router.post('/login',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email').normalizeEmail(),
+    body('password', 'Please enter a password with only numbers and text and atleast 6 characters').trim()
+      .isLength({ min: 6 })
+      .isAlphanumeric()
+  ], authController.postLogin);
 
 // /signup -> GET
+router.get('/signup', authController.getSignup);
 
 // /signup -> POST
+router.post('/signup', 
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value })
+          .then(userDoc => {
+            if (userDoc) {
+              return Promise.reject('A user corresponding to this email already exists')
+            }
+          })
+      })
+      .normalizeEmail(),
+    body('password', 'Please enter a password with only numbers and text and atleast 6 characters')
+      .isLength({ min: 6 })
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword').trim().custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    })
+  ], authController.postSignup);
 
 // /logout -> POST
+router.post('/logout', authController.postLogout);
 
 // /password-reset -> GET
 
@@ -17,3 +58,5 @@
 // /password-reset/:token -> GET
 
 // /new-password -> POST
+
+module.exports = router;
