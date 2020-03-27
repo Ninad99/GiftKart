987b6f6d0291af2ad94800d14d6b5b3c941ad384
ninad99 <ninuch7@gmail.com>
2020-03-27 12:47:58 +0530

    Added user sessions using mongoDB, and added admin and auth routes

--- app.js
+++ app.js
@@ -7,27 +7,62 @@ require('dotenv').config();
 const DATABASE_URI = process.env.MONGODB_URI;
 const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
 const errorController = require('./controllers/error');
+
+const User = require('./models/user');
+const store = new mongoDBStore({
+  uri: DATABASE_URI,
+  collection: 'sessions'
+}, (err) => {
+  console.log(err);
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// TODO --> add routes and uncomment the following
-// const adminRoutes = require('./routes/admin');
-// const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
+const authRoutes = require('./routes/auth');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'giftkart',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 // 1 hour
+  },
+  store: store
+}));
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = false;
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
+    if (!user) {
+      return next();
+    }
+    req.user = user;
+    next();
+  })
+  .catch(err => {
+    throw new Error(err);
+  })
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.isAdmin = req.session.isAdmin;
   next();
 });
 
-// Uncomment above routes before add the following
-// app.use('/admin', adminRoutes);
-// app.use(authRoutes);
+app.use('/admin', adminRoutes);
+app.use(authRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
