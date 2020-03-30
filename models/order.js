9bec25afc1e573d31e3898bb127cbcbcc8fdbf16
ninad99 <ninuch7@gmail.com>
2020-03-30 16:22:16 +0530

    Order model created

---
+++ models/order.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  products: [
+    {
+      productData: {
+        type: Object,
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  user: {
+    email: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
+  },
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  orderDate: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
