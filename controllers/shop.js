65450a5bb702e7f0f20244b4543768b249d52c00
Avishek Mondal <avishek.mondal53@gmail.com>
2020-04-02 19:05:21 +0530

    added recomd page and its functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -124,10 +124,55 @@ exports.getOrders = (req, res, next) => {
 }
 
 exports.postRecommendProducts = (req, res, next) => {
-	console.log(req.body);
-	//handle further
+  let query;
 
-	Product.find()
+  // code to handle ages
+  if(req.body.minage !== '0'){
+    if(req.body.maxage !== '0'){
+      query = {
+        'ages.min' : req.body.minage,
+        'ages.max' : req.body.maxage
+      }
+    }else{
+      query = {
+        'ages.min' : req.body.minage
+      }
+    }
+  }else{
+    query = {};
+  }
+
+  // code to handle occasions
+  if(req.body.occasion) {
+    if(Array.isArray(req.body.occasion)) {
+      let QueryOccasions = [];
+      for(let i in req.body.occasion) {
+        QueryOccasions.push(req.body.occasion[i]);
+      }
+      query.occasion = QueryOccasions;
+    } else {
+      query.occasion =  req.body.occasion;
+    } 
+  }
+
+  // code to handle price
+  if(req.body.minprice && req.body.maxprice) {
+    query.price = { $gt: req.body.minprice, $lt: req.body.maxprice};
+  }
+  else if(req.body.minprice) {
+    query.price = { $gt: req.body.minprice};
+  }
+  else if(req.body.maxprice){
+    query.price = { $lt: req.body.maxprice};
+  }
+
+  // code to handle gender
+  if(req.body.gender){
+    let queryGender = [req.body.gender, 'B'];
+    query.gender = queryGender;
+  }
+
+	Product.find(query)
 		.then(products => {
 			return res.render("shop/recommend-products", {
 				prods: products,
