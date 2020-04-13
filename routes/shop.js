5778a75d9f829104ecb26ffc927d2bf9a8f9078b
ninad99 <ninuch7@gmail.com>
2020-04-13 21:51:23 +0530

    Order form validation

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
 const shopController = require('../controllers/shop');
 const isAuth = require('../middleware/is-auth');
@@ -26,7 +27,14 @@ router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 router.get('/orders', isAuth, shopController.getOrders);
 
 // /create-order -> POST
-router.post('/create-order', isAuth, shopController.postOrder);
+router.post('/create-order',
+  [
+    body('name').isString().notEmpty().trim(),
+    body('house').isString().notEmpty().trim(),
+    body('street').isString().notEmpty().trim(),
+    body('city').isString().notEmpty().trim(),
+    body('PIN').isNumeric().isLength({ min: 6, max: 6 })
+  ], isAuth, shopController.postOrder);
 
 // /recommend-products -> POST 
 router.post("/recommend-products", shopController.postRecommendProducts);
